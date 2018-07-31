22dc61486329f1f3cfc77fd43f6c9921413565c3
aecreations <aeng.aecreations@gmail.com>
2018-07-30 20:52:13 -0700

    Added ability to update config file; Hungarian notation on global variables

--- src/syncClippings.py
+++ src/syncClippings.py
@@ -8,53 +8,72 @@ import json
 import struct
 import configparser
 
-debug = True
+DEBUG = True
 
-appName = "Sync Clippings"
-appInternalName = "syncClippings"
-appVer = "1.0a0+"
-confFilename = "syncClippings.ini"
-syncFilename = "clippings-sync.json"
-defaultClippingsData = {
+gAppName = "Sync Clippings"
+gAppInternalName = "syncClippings"
+gAppVer = "1.0a0+"
+gConfFilename = "syncClippings.ini"
+gSyncFilename = "clippings-sync.json"
+gDefaultClippingsData = {
     "version": "6.0",
     "createdBy": "Sync Clippings",
     "userClippingsRoot": []
 }
 
-def getSyncFilePath():
-    conf = configparser.ConfigParser()
-    conf.read(confFilename)
-    return conf["Sync File"]["Path"]
-
 def getAppName():
-    return appName
+    return gAppName
 
 def getAppVer():
-    return appVer
+    return gAppVer
 
-def log(msg):
-    if debug:
-        with open("debug.txt", "a") as file:
-            file.write(msg)
-            file.write("\n")
+def getSyncFilePath():
+    conf = configparser.ConfigParser()
+    conf.read(gConfFilename)
+    rv = conf["Sync File"]["Path"]
+    return rv
+
+def setSyncFilePath(aPath):
+    conf = configparser.ConfigParser()
+    conf["Sync File"] = { "Path": aPath }
+    with open(gConfFilename, "w") as configFile:
+        conf.write(configFile)
     
+def log(aMsg):
+    if DEBUG:
+        with open("debug.txt", "a") as file:
+            file.write(aMsg)
+            file.write("\n")
+
+def getResponseOK():
+    rv = { "status": "ok" }
+    return rv
+
+def getResponseErr(aErr):
+    template = "Exception {0} (arguments: {1!r})"
+    rv = {
+        "status": "failure",
+        "details": template.format(type(aErr).__name__, aErr.args)
+    }
+    return rv
+
 def getMessage():
     rawLength = sys.stdin.buffer.read(4)
     if len(rawLength) == 0:
         sys.exit(0)
     messageLength = struct.unpack('@I', rawLength)[0]
     message = sys.stdin.buffer.read(messageLength).decode('utf-8')
-    jsonDict = json.loads(message)   
-    return jsonDict
+    rv = json.loads(message)   
+    return rv
 
-def encodeMessage(messageContent):
-    encodedContent = json.dumps(messageContent).encode('utf-8')
+def encodeMessage(aMsgContent):
+    encodedContent = json.dumps(aMsgContent).encode('utf-8')
     encodedLength = struct.pack('@I', len(encodedContent))
     return {'length': encodedLength, 'content': encodedContent}
 
-def sendMessage(encodedMessage):
-    sys.stdout.buffer.write(encodedMessage['length'])
-    sys.stdout.buffer.write(encodedMessage['content'])
+def sendMessage(aEncodedMsg):
+    sys.stdout.buffer.write(aEncodedMsg['length'])
+    sys.stdout.buffer.write(aEncodedMsg['content'])
     sys.stdout.buffer.flush()
 
     
@@ -65,7 +84,7 @@ while True:
     if "msgID" not in msg:
         err = "Error: expected key 'msgID' does not exist!"
         log(err)
-        sys.stderr.buffer.write("%s: %s" % (appInternalName, err))
+        sys.stderr.buffer.write("%s: %s" % (gAppInternalName, err))
         sys.stderr.buffer.flush()
         sys.exit(1)
 
@@ -80,7 +99,15 @@ while True:
         resp = {
             "syncFilePath": getSyncFilePath()
         }
+    elif msg["msgID"] == "set-sync-file-path":
+        path = msg["filePath"]
+        log("Message 'set-sync-file-path': filePath = {0}".format(msg['filePath']))
+        try:
+            setSyncFilePath(path)
+            resp = getResponseOK()
+        except Exception as e:
+            resp = getResponseErr(e)
 
-    if resp != None:
+    if resp is not None:
         sendMessage(encodeMessage(resp))
 
