702669c625c619730a65495bb421d532b82036e5
AE Creations <aecreations@users.noreply.github.com>
2025-10-11 22:55:49 -0700

    Changed application version to 2.0.1b2+

--- src/syncClippings.py
+++ src/syncClippings.py
@@ -20,7 +20,7 @@ from tkinter import filedialog
 DEBUG = False
 
 APP_NAME = "Sync Clippings"
-APP_VER = "2.0.1b2"
+APP_VER = "2.0.1b2+"
 CONF_FILENAME = "syncClippings.ini"
 SYNC_FILENAME = "clippings-sync.json"
 
